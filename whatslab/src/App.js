03c85e58b74a1310fe5a2082f9842db7d805482e
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-05-15 13:38:20 -0300

    WhatsLab - Início do início

---
+++ whatslab/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <>
+      <div className="area-post">
+      ÁREA DE POSTAGEM
+      </div>
+      
+      
+      <div className="inputs">
+      <input placeholder="Usuário"></input>
+      <input placeholder="Mensagem"></input>
+      <button>Enviar</button>
+      </div>
+      
+      
+    </>
+  );
+}
+
+export default App;
